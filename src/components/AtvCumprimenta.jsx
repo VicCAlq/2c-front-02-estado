8fd9e62da0b2f852c26dc3583a40baa77a433c87
thuliobarbosa-creator <thulio.barbosa@nave.org.br>
2026-06-15 11:54:03 -0300

    atividade

--- src/components/AtvCumprimenta.jsx
+++ src/components/AtvCumprimenta.jsx
@@ -19,3 +19,67 @@ _________________________________
 | É um prazer lhe conhecer...   |
 |_______________________________|
 */
+import { useState } from 'react'
+
+// Estilos de "CSS"
+const estilo = {
+  view: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "10px",
+    backgroundColor: "#303540",
+    padding: "10px",
+    margin: "10px",
+    gap: "10px",
+  },
+  texto: {
+    color: "#eee",
+    fontSize: "32px",
+  },
+  textoBotao: {
+    color: "#eee",
+    fontSize: "16px",
+  },
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+  },
+  viewBotoes: {
+    display: "flex",
+    flexDirection: "row",
+    flex: "1 0",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  input: {
+    borderRadius: "5px",
+    backgroundColor: "#bbb",
+    padding: "4px",
+  }
+}
+
+export default function AtvCumprimenta() {
+  // Criação das variáveis que armazenam o texto digitado, e o texto salvo
+    const [textoInserido, setTextoInserido] = useState("")
+    const [texto, setTexto] = useState("Qual é seu nome?")
+    return(
+        <div style={estilo.view}>
+        <p style={estilo.texto}>
+            {texto}
+        </p>
+        <input
+            type="text"
+            value={textoInserido}
+            style={estilo.input}
+            onChange={(e) => setTextoInserido(e.target.value)}
+            placeholder="Digite aqui seu nome"
+        />
+        <button style={estilo.botao} onClick={() => setTexto(textoInserido)}>
+            <p style={estilo.textoBotao}>Salvar</p>
+        </button>
+        </div>
+    )
+}
--- src/components/AtvDobra.jsx
+++ src/components/AtvDobra.jsx
@@ -5,3 +5,60 @@ Crie e exporte por padrão um componente chamado "AtvDobra" que recebe um argume
 O componente deve exibir uma <div> contendo um <p>, que vai exibir este número recebido como argumento, 
 e um <button>, que ao ser clicado, modifica o valor do número para dobrar o valor dele.
 */
+import { useState } from 'react'
+
+// Estilos de "CSS"
+const estilo = {
+  view: {
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: "10px",
+    backgroundColor: "#303540",
+    padding: "10px",
+    margin: "10px",
+    gap: "10px",
+  },
+  texto: {
+    color: "#eee",
+    fontSize: "32px",
+  },
+  textoBotao: {
+    color: "#eee",
+    fontSize: "16px",
+  },
+  botao: {
+    borderRadius: "5px",
+    backgroundColor: "#505560",
+    padding: "10px",
+    margin: "10px",
+  },
+  viewBotoes: {
+    display: "flex",
+    flexDirection: "row",
+    flex: "1 0",
+    width: "100%",
+    justifyContent: "space-between",
+    alignItems: "center",
+  }
+}
+
+export default function AtvDobra({numero}) {
+  const [valor1, setNumeroDobrado] = useState(numero)
+
+  function dobrar() {
+    setNumeroDobrado(valor1 * 2)
+  }
+   return(<>
+    <div style={estilo.view}>
+      <p style={estilo.texto}>
+        {valor1}
+      </p>
+      <div>
+        <button style={estilo.botao} onClick={dobrar}>
+          <p style={estilo.textoBotao}>Dobrar</p>
+        </button>
+      </div>
+    </div>
+</>)
+}
+
