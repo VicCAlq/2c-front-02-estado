f435099e42b012dfbe71e9090ded5445b2215459
pedro-arthur-nave <pedro.arthur@nave.org.br>
2026-06-15 11:41:24 -0300

    Atualização 01

--- src/components/AtvDobra.jsx
+++ src/components/AtvDobra.jsx
@@ -5,3 +5,33 @@ Crie e exporte por padrão um componente chamado "AtvDobra" que recebe um argume
 O componente deve exibir uma <div> contendo um <p>, que vai exibir este número recebido como argumento, 
 e um <button>, que ao ser clicado, modifica o valor do número para dobrar o valor dele.
 */
+
+import React, { useState } from 'react';
+import { View, texto, botao, textoBotao, viewBotoes } from 'react-native';
+
+const estilos = create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    },
+  text: {
+    fontSize: 16,
+    marginBottom: 25,
+  },
+    });
+
+export default function App(AtvDobra)
+    {
+const [numero, setNumero] = useState("2px");
+    }
+function dobrar() {
+    setNumero(numero * 2);
+    }
+
+     return (
+    <View style={estilos.container}>
+      <Text style={estilos.text}> sim {numero} vezes!</Text>
+      <Button title="Click here" onPress={dobrar} />
+    </View>
+    );
