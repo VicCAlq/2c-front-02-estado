0d53e806d2b33112fe3c8c84e232b981db500437
caue-ferreira-nave <caue.ferreira@nave.org.br>
2026-06-15 11:35:15 -0300

    dobraentre

--- src/components/AtvDobra.jsx
+++ src/components/AtvDobra.jsx
@@ -5,3 +5,21 @@ Crie e exporte por padrão um componente chamado "AtvDobra" que recebe um argume
 O componente deve exibir uma <div> contendo um <p>, que vai exibir este número recebido como argumento, 
 e um <button>, que ao ser clicado, modifica o valor do número para dobrar o valor dele.
 */
+
+
+import { useState } from 'react';
+
+export default function AtvDobra({ valor }) {
+  const [numero, setNumero] = useState(valor);
+
+  function dobrarNumero() {
+    setNumero(numero * 2);
+  }
+
+  return (
+    <div>
+      <p>{numero}</p>
+      <button onClick={dobrarNumero}>Dobrar</button>
+    </div>
+  );
+}
