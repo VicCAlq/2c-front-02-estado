c31d3bd66bcc8e5d163843182bba5663ed376b9e
mateusnave <mateus.antonio@nave.org.br>
2026-06-15 11:49:36 -0300

    DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD

--- src/components/AtvTelefoneSemFio.jsx
+++ src/components/AtvTelefoneSemFio.jsx
@@ -7,3 +7,59 @@ Cada <button> ao ser clicado, deve inserir na <div> abaixo um <p> e um <input>,
 e substituir o conteúdo anterior dessa <div> de baixo dos botões.
 O texto digitado no <input> ativado por um <button> deve ser exibido no <p> do outro <button>, e vice-versa.
 */
+
+import { useState } from "react";
+
+export default function AtvTelefoneSemFio() {
+
+    const [conteudoClaro, setConteudoClaro] = useState('');
+    const [conteudoOi, setConteudoOi] = useState('');
+    const [telefone, setTelefone] = useState('');
+
+    const style = {
+        box: {
+            display: 'flex',
+        }
+    }
+
+    return (
+        
+        <div style={style.box}>
+            <button onClick={() => setTelefone('claro')}>
+                Telefone Claro
+            </button>
+            <button onClick={() => setTelefone('oi')}>
+                Telefone Oi
+            </button>
+
+            
+
+            <div>
+                {telefone === 'claro' ? (
+                    <>
+                    <input 
+                        type="text"
+                        onChange={(e) => setConteudoClaro(e.target.value)}
+                        placeholder="Telefone de Vitinho"
+                        value={conteudoOi}
+                    />
+                    </>
+                ) : null}
+
+                {telefone === 'oi' ? (
+                    <>
+                    <input 
+                        type="text"
+                        onChange={(e) => setConteudoOi(e.target.value)}
+                        placeholder="Telefone de Silviooo"
+                        value={conteudoClaro}
+                    />
+                    </>
+                ) : null}
+
+
+            </div>
+            
+        </div>
+    );
+}
