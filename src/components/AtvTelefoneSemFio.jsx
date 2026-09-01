52a402bdfa90a871707071ff58e49230d3c1282d
gustavoalberto-create <gustavo.alberto@nave.org.br>
2026-09-01 13:10:51 -0300

    att4

--- src/components/AtvTelefoneSemFio.jsx
+++ src/components/AtvTelefoneSemFio.jsx
@@ -7,3 +7,46 @@ Cada <button> ao ser clicado, deve inserir na <div> abaixo um <p> e um <input>,
 e substituir o conteúdo anterior dessa <div> de baixo dos botões.
 O texto digitado no <input> ativado por um <button> deve ser exibido no <p> do outro <button>, e vice-versa.
 */
+
+import { useState } from "react";
+
+function AtvTelefoneSemFio() {
+  const [texto1, setTexto1] = useState("");
+  const [texto2, setTexto2] = useState("");
+  const [ativo, setAtivo] = useState(null);
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setAtivo(1)}>Botão 1</button>
+        <button onClick={() => setAtivo(2)}>Botão 2</button>
+      </div>
+
+      <div>
+        {ativo === 1 && (
+          <>
+            <p>{texto2}</p>
+            <input
+              type="text"
+              value={texto1}
+              onChange={(e) => setTexto1(e.target.value)}
+            />
+          </>
+        )}
+
+        {ativo === 2 && (
+          <>
+            <p>{texto1}</p>
+            <input
+              type="text"
+              value={texto2}
+              onChange={(e) => setTexto2(e.target.value)}
+            />
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default AtvTelefoneSemFio;
